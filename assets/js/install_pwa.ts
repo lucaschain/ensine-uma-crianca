44437312db10125efb0b4d080c2d9ed3762cb67c
Lucas Chain <sou@lucaschain.com.br>
2025-03-26 14:41:03 -0300

    fix: pwa install e mode

--- assets/js/install_pwa.ts
+++ assets/js/install_pwa.ts
@@ -1,26 +1,26 @@
 export function registerBeforeInstallPwa() {
-  // let deferredPrompt;
-  // const decided = localStorage.getItem('install-pwa-decided');
-  // window.addEventListener('beforeinstallprompt', async (e) => {
-  //   e.preventDefault();
-  //   deferredPrompt = e;
-  //   const alert = document.getElementById('install-alert');
-  //
-  //   if (!alert || decided) {
-  //     return;
-  //   }
-  //   alert.classList.remove('hidden');
-  //
-  //   const noButton = document.getElementById('install-alert-no');
-  //   noButton?.addEventListener('click', () => {
-  //     alert.classList.add('hidden');
-  //     localStorage.setItem('install-pwa-decided', 'true');
-  //   });
-  //
-  //   const yesButton = document.getElementById('install-alert-yes');
-  //   yesButton?.addEventListener('click', () => {
-  //     alert.classList.add('hidden');
-  //     deferredPrompt.prompt();
-  //   });
-  // });
+  let deferredPrompt;
+  const decided = localStorage.getItem('install-pwa-decided');
+  window.addEventListener('beforeinstallprompt', async (e) => {
+    e.preventDefault();
+    deferredPrompt = e;
+    const alert = document.getElementById('install-alert');
+
+    if (!alert || decided) {
+      return;
+    }
+    alert.classList.remove('hidden');
+
+    const noButton = document.getElementById('install-alert-no');
+    noButton?.addEventListener('click', () => {
+      alert.classList.add('hidden');
+      localStorage.setItem('install-pwa-decided', 'true');
+    });
+
+    const yesButton = document.getElementById('install-alert-yes');
+    yesButton?.addEventListener('click', () => {
+      alert.classList.add('hidden');
+      deferredPrompt.prompt();
+    });
+  });
 }
